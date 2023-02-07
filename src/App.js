ac37988714e1b14092fffd820adfb1093dc6f8f1
Hafeefa <hafeefa40@gmail.com>
2023-02-07 14:19:57 -0500

    Made code neater

--- src/App.js
+++ src/App.js
@@ -18,25 +18,25 @@ function App() {
     <>
    
     <div className="normalfont">
-    {/* id="backgroundColor" */}
    
     <Navbarr></Navbarr>
 <br/> <br/> <br/>
     
 <Container>
 
-      {/* <Row> */}
-      {/* <Col style={{paddingTop:"150px"}}> */}
             <h1 className="normalfont"> <strong></strong> </h1>
               <h3 style=
                 {{color:'black' , 
-                fontSize:"35px", paddingLeft:"30px" ,
-                paddingBottom:"50px", textAlign:"center", 
+                fontSize:"35px", 
+                paddingLeft:"30px" ,
+                paddingBottom:"50px", 
+                textAlign:"center", 
                 borderRadius:"20px"}}
                 className="underlineintro"> 
                 Hello world, nice to meet you! My name is <em style={{textDecoration:"underline"}}>Hafeefa Sultan</em> 🎀 </h3>
                 
-              <h5 style={{color:'black',
+              <h5 style=
+              {{color:'black',
               padding:"20px", 
               textAlign:"center", 
               textDecoration:"underline",
@@ -49,32 +49,39 @@ function App() {
         <div className="image-fluid">
            <img src="https://i.pinimg.com/originals/01/9f/c4/019fc468dd8f365bd036e228772eb66d.jpg"
              id="art"/>
-             </div>
-             <br/>
+       </div>
+            
+            <br/>
     
 {/* </div> */}
       <h1 
-        style={{textAlign:"center", 
+        style=
+        {{textAlign:"center", 
         paddingTop:"15px", 
         fontSize:"25px"}}>
-      <em style={{ textDecoration: "underline", 
-        color:"black" ,padding:"10px"}}
-      className="underlineintro">
-        Learn more about me below!
-        </em>
-         </h1> 
-            <br/> <br/>
+          
+          <em style=
+          {{textDecoration: "underline", 
+            color:"black",
+            padding:"10px"}}
+              className="underlineintro">
+                 Learn more about me below!
+          </em>
+      </h1> 
+
+             <br/> <br/>
              <br/> <br/>
              <br/> <br/>
              <br/>
+             
        {/* first card */}
 
       <Row style= {{paddingTop:"10px"}}>
       <p id="a" style={{textAlign:"center", fontSize:"45px", paddingBottom:"10px"}}> 
           Frameworks and Languages I use
-       </p>
+      </p>
 
-        <Col style={{marginLeft:"10px"}}><Card  id="cards">
+    <Col style={{marginLeft:"10px"}}><Card  id="cards">
       <Card.Body>
         <Card.Title id="cardTitleSpacing">HTML, CSS, JavaScript</Card.Title>
         <Card.Text>
@@ -95,8 +102,7 @@ function App() {
 
     {/* second card */}
 
-        <Col  style={{marginLeft:"10px"}}><Card  id="cards" >
-      {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+    <Col  style={{marginLeft:"10px"}}><Card  id="cards" >
       <Card.Body>
         <Card.Title id="cardTitleSpacing">React JS, BootStrap JS</Card.Title>
         <Card.Text>
@@ -110,14 +116,12 @@ function App() {
           id="picture" style={{height:"14vh", marginTop:"35px"}}/>
 
         </Card.Text>
-        {/* <Button variant="primary">Go somewhere</Button> */}
       </Card.Body>
     </Card></Col>
     <br/>
 
     {/* third card */}
-        <Col  style={{marginLeft:"10px"}}><Card  id="cards">
-      {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+    <Col  style={{marginLeft:"10px"}}><Card  id="cards">
       <Card.Body>
         <Card.Title id="cardTitleSpacing">Java</Card.Title>
         <Card.Text>
@@ -130,13 +134,13 @@ function App() {
         </Card.Text>
       </Card.Body>
     </Card></Col>
-      </Row>
+  </Row>
 <br/><br/> <br/> <br/> <br/> <br/> <br/>
 
     <p id="a" 
           className="underlineintro"
           style={{paddingBottom:"10px",fontSize:"50px", textAlign:"center"}}>
-        Learn more about me and my objectives!
+          Learn more about me and my objectives!
     </p>
 <br/>
 {/* my own picture */}
@@ -177,8 +181,13 @@ function App() {
 
       {/* beginning of why exactly */}
 
-<div style={{border:"solid black", borderRadius:"10px", borderWidth:"5px",padding:"45px",
- maxWidth:"100%" , height:"auto"}}>
+<div 
+  style={{border:"solid black", 
+  borderRadius:"10px", 
+  borderWidth:"5px",
+  padding:"45px",
+  maxWidth:"100%", 
+  height:"auto"}}>
 
     <br/>   
       <p 
@@ -196,17 +205,17 @@ function App() {
         <Card.Title className="underlineintro" id= "secondCardTitle" style={{fontSize:"35px", color:"black"}} > 
           <em> It's fun! </em>
           </Card.Title>
-        <Card.Text style={{ paddingTop:"6px", fontSize:"18px", color:"black"}}>
-          During an internship I had during January 2023, I worked in a team of 5 and we
-          were able to build a complete marketplace full-stack website. 
-          Building strong connections, worrking together and striving for the same goal
-           made the internship so much fun. That's one of the highlights
-          of being a software engineer, working in cooperative teams and aiming for a common target!
-        </Card.Text>
+            <Card.Text style={{ paddingTop:"6px", fontSize:"18px", color:"black"}}>
+              During an internship I had during January 2023, I worked in a team of 5 and we
+              were able to build a complete marketplace full-stack website. 
+              Building strong connections, worrking together and striving for the same goal
+              made the internship so much fun. That's one of the highlights
+              of being a software engineer, working in cooperative teams and aiming for a common target!
+            </Card.Text>
       </Card.Body>
     </Card>
-    <br/>
-    <br/> 
+
+    <br/><br/> 
     {/* second */}
 
   
@@ -216,14 +225,12 @@ function App() {
       {/* <Card.Header as="h5">Featured</Card.Header> */}
       <Card.Body>
         <Card.Title  id= "secondCardTitle" className="underlineintro" style={{fontSize:"35px"}} > <em>I love to problem solve!</em></Card.Title>
-        <Card.Text style={{paddingTop:"6px",  fontSize:"18px", color:"black"}}>
-
+          <Card.Text style={{paddingTop:"6px",  fontSize:"18px", color:"black"}}>
           I personally enjoy problem solving
           very much. I love the challenge it gives to my brain, and I learned I really work best when working in 
           a problem solving environment! When I find the solution to my problems, it just makes me want
           to celebrate!
-
-        </Card.Text>
+          </Card.Text>
       </Card.Body>
     </Card>
     <br/>
@@ -245,44 +252,39 @@ function App() {
     <CardGroup >
       <Card className="bg-light" style={{maxWidth: "100%", height: "auto",  borderWidth:"2px"}} >
         <Card.Img variant="top" src="https://www.makerstations.io/content/images/2022/08/jb-bardoles-desk-setup-02.jpg" />
-        <Card.Body>
-          <Card.Title style={{color:"black", fontSize:"30px", textDecoration:"underline", paddingBottom:"5px"}} 
-          className="underlineintro"> 
-            What I used in order to build this website</Card.Title>
-          <Card.Text style={{paddingTop:"10px", fontSize:"20px", color:"black"}}>
-            <ul>
-              <li> Editor: VSCode</li>
-              <li> Framework: Bootstrap </li>
-              <li> Open source: Node.js, Git, Github </li>
-              <li> Languages: React, HTML, JavaScript, CSS </li>
-            </ul>
+          <Card.Body>
+            <Card.Title style={{color:"black", fontSize:"30px", textDecoration:"underline", paddingBottom:"5px"}} 
+              className="underlineintro"> 
+                What I used in order to build this website</Card.Title>
+         
+                <Card.Text style={{paddingTop:"10px", fontSize:"20px", color:"black"}}>
+                  <ul>
+                    <li> Editor: VSCode</li>
+                    <li> Framework: Bootstrap </li>
+                    <li> Open source: Node.js, Git, Github </li>
+                    <li> Languages: React, HTML, JavaScript, CSS </li>
+                  </ul>
           </Card.Text>
         </Card.Body>
-         {/* <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>  */}
       </Card> 
 
       {/* second card */}
 
       <Card className="bg-light" style={{maxWidth: "100%", height: "auto",  borderWidth:"2px"}} >
         <Card.Img variant="top" src="https://www.makerstations.io/content/images/size/w1384/2022/08/jb-bardoles-desk-setup-01.jpg" />
-        <Card.Body>
-          <Card.Title style={{color:"black", fontSize:"30px", textDecoration:"underline", paddingBottom:"5px"}}
-           className="underlineintro">
-            My experience with building sites </Card.Title>
+          <Card.Body>
+              <Card.Title style={{color:"black", fontSize:"30px", textDecoration:"underline", paddingBottom:"5px"}}
+                className="underlineintro">
+                   My experience with building sites </Card.Title>
 
           <Card.Text  style={{paddingTop:"10px", fontSize:"20px",color:"black"}}>
-            <li > 9 months experience with front end</li>
-            <li > One web development certificate</li>
-            <li > Have built a full stack marketplace website for business processes in a team of five
-              in under three weeks
+              <li > 9 months experience with front end</li>
+              <li > One web development certificate</li>
+              <li > Have built a full stack marketplace website for business processes in a team of five
+                in under three weeks
             </li>
           </Card.Text>
         </Card.Body>
-        {/* <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer> */}
       </Card>
 
     </CardGroup>
@@ -292,17 +294,16 @@ function App() {
 <br/><br/> 
 <br/> <br/>
 
-   
-   
-   <ContactUs/>
-   <br/>
-      <p style={{textAlign:"center", fontSize:"12px"}}>
-        <em>Desgined and developed by Hafeefa Sultan on February 6, 2023. </em> 
-      </p>
-    <br/>
-    </Container>
 
-   </div>
+        <ContactUs/>
+          <br/>
+              <p style={{textAlign:"center", fontSize:"12px"}}>
+                <em>Desgined and developed by Hafeefa Sultan on February 6, 2023. </em> 
+              </p>
+            <br/>
+        </Container>
+      </div>
+
     </>
   );
 }
